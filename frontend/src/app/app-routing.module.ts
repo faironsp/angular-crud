e8be9e7d20124e19c6db9718b82b4558db696713
Fabio de Oliveira Andrade <faironsp@gmail.com>
2020-06-04 12:34:34 -0300

    mudando para user

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductCreateComponent } from './components/product/user-create/user-create.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/user-update/user-update.component';
+import { ProductDeleteComponent } from './components/product/user-delete/user-delete.component';
 
 const routes: Routes = [
   {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,21 +13,21 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductCreateComponent } from './components/product/user-create/user-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductReadComponent } from './components/product/user-read/user-read.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/user-update/user-update.component';
+import { ProductDeleteComponent } from './components/product/user-delete/user-delete.component';
 
 registerLocaleData (localePt);
 
